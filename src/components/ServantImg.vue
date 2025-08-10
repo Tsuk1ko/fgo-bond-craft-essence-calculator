33c4c41374717d3cd4b20812f0a2d50200c4b349
神代綺凛 <i@loli.best>
2025-08-10 17:29:48 +0800

    feat: 图片加载骨架屏

--- src/components/ServantImg.vue
+++ src/components/ServantImg.vue
@@ -1,5 +1,18 @@
 <template>
-  <img class="servant-img" :src="src" :alt="name" />
+  <div class="servant-img">
+    <img width="100%" height="100%" :src="src" :alt="name" @load="loading = false" />
+    <el-skeleton
+      v-if="loading"
+      class="servant-img-skeleton"
+      :loading="true"
+      :throttle="500"
+      animated
+    >
+      <template #template>
+        <el-skeleton-item class="servant-img-skeleton-item" variant="image" />
+      </template>
+    </el-skeleton>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -7,6 +20,8 @@ const { id, name } = defineProps<{
   id: number;
   name?: string;
 }>();
+
+const loading = ref(true);
 
 const src = computed(() => `assets/servant/Servant${id.toString().padStart(3, '0')}.jpg`);
 </script>
@@ -17,5 +32,17 @@ const src = computed(() => `assets/servant/Servant${id.toString().padStart(3, '0
   height: 70px;
   flex-grow: 0;
   flex-shrink: 0;
+}
+
+.servant-img-skeleton {
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  &,
+  &-item {
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
